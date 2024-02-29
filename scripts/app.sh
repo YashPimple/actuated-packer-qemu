be1d7a86398896029f26fbc1eded16c2ea8b7dbd
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2024-02-29 18:28:49 +0530

    Update app.sh

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-sleep 30
-
-sudo yum update -y
 
 sudo apt update
+sudo apt install -y python3
 sudo apt install -y python3-pip
-pip3 install torch==2.2.0
+
+sudo apt install -y python3-venv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install keras
+
+python -c "import keras; print(keras.__version__)"
+
+name=$IMAGE_NAME
+version=$IMAGE_VERSION
+image="${name}${version}"
+path_image="artifacts/qemu/${image}"
+
+file_ext=$IMAGE_FORMAT
+if [ "$format" = "raw" ]; then
+  file_ext="img"
+fi
+
+# go to the artifact folder
+cd ${path_image}
+
+# rename the image, compute shasum
+mv packer-${image} ${image}.${file_ext}
+sha256sum ${image}.${file_ext} > ${image}.${file_ext}.sha256sum
