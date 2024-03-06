606699bc3df3172f672ef044d0d72eb295ff6b6f
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2024-03-06 12:47:57 +0530

    Update app.sh

--- scripts/app.sh
+++ scripts/app.sh
@@ -4,6 +4,22 @@ sleep 30
 
 sudo yum update -y
 
-sudo apt update
+sudo apt update -y python3-pip libgl1-mesa-glx
 sudo apt install -y python3-pip
-pip3 install torch==2.2.0
+pip3 install torch==2.2.0 torchvision torchaudio
+pip3 install opencv-python
+
+name=$IMAGE_NAME
+version=$IMAGE_VERSION
+image="${name}${version}"
+path_image="artifacts/qemu/${image}"
+
+file_ext=$IMAGE_FORMAT
+if [ "$format" = "raw" ]; then
+  file_ext="img"
+fi
+
+cd ${path_image}
+
+mv packer-${image} ${image}.${file_ext}
+sha256sum ${image}.${file_ext} > ${image}.${file_ext}.sha256sum
